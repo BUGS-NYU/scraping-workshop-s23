0eae193a6efea97e7ccb66b89a8265326df6a8b7
Calvin Tian <48587962+calvinatian@users.noreply.github.com>
2023-03-24 16:36:43 -0400

    CHG update demo

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,26 @@ async function main() {
     const page = await browser.newPage();
     page.setDefaultTimeout(1000000);
 
-    // fs.writeFile('books.json', '', function (err) {
-    //     if (err) throw err;
-    // });
+    // page.pause();  // uncomment to launch debugger for playwright
 
+    // navigate to books catalogue page 1
     await page.goto("https://books.toscrape.com/catalogue/page-1.html");
 
-    await page.waitForTimeout(1000);
-    await browser.close();
+    const allBooks = await ...;  // TODO: replace "..." with code to get all the books (use .all() on a locator or getByRole)
+
+    // for each of the book elements, get the title and price
+    for (const book of allBooks) {
+        const bookTitle = await ...;  // TODO: replace "..." with code to get the book title
+        console.log(bookTitle)
+
+        // now do the same for the price
+
+    };
+
+
+    // uncomment below if you want to automatically close the browser window when the script finishes
+    // await page.waitForTimeout(1000);
+    // await browser.close();
 }
 
 main()
